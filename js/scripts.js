8913e19910026fbfb68995bfe8d8048c64cc0266
piersrueb <piersrueb@gmail.com>
2018-09-28 22:20:31 +0100

    init

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,13 @@
 //  scroll it
 
 function scrollIt(destination, duration = 200, easing = 'linear', callback) {
-
-    const easings = {
-        linear(t) {
-            return t;
-        },
-        easeInQuad(t) {
-            return t * t;
-        },
-        easeOutQuad(t) {
-            return t * (2 - t);
-        },
-        easeInOutQuad(t) {
-            return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
-        },
-        easeInCubic(t) {
-            return t * t * t;
-        },
-        easeOutCubic(t) {
-            return (--t) * t * t + 1;
-        },
-        easeInOutCubic(t) {
-            return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
-        },
-        easeInQuart(t) {
-            return t * t * t * t;
-        },
-        easeOutQuart(t) {
-            return 1 - (--t) * t * t * t;
-        },
-        easeInOutQuart(t) {
-            return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t;
-        },
-        easeInQuint(t) {
-            return t * t * t * t * t;
-        },
-        easeOutQuint(t) {
-            return 1 + (--t) * t * t * t * t;
-        },
-        easeInOutQuint(t) {
-            return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t;
-        }
-    };
-
+    const easings = { easeInOutQuart(t) { return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * (--t) * t * t * t; } };
     const start = window.pageYOffset;
     const startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
     const documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
     const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
     const destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop;
     const destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset);
-
     if ('requestAnimationFrame' in window === false) {
         window.scroll(0, destinationOffsetToScroll);
         if (callback) {
@@ -58,7 +15,6 @@ function scrollIt(destination, duration = 200, easing = 'linear', callback) {
         }
         return;
     }
-
     function scroll() {
         const now = 'now' in window.performance ? performance.now() : new Date().getTime();
         const time = Math.min(1, ((now - startTime) / duration));
@@ -72,7 +28,6 @@ function scrollIt(destination, duration = 200, easing = 'linear', callback) {
         }
         requestAnimationFrame(scroll);
     }
-
     scroll();
 }
 
@@ -122,6 +77,12 @@ function buildNav(){
     }
 }
 buildNav();
+
+//  nav position
+
+var navHeight = document.getElementById('dot-nav').clientHeight,
+    hNavHeight = navHeight / 2;
+document.getElementById('dot-nav').style.top = 'calc(50% - ' + hNavHeight + 'px)';
 
 //  onscroll
 
